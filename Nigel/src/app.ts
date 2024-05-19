0e6420e682bf6cbd286252f7fd2a6125f7918c4f
Ali Azam <aliaccounts@azam.email>
2024-05-19 01:00:37 -0400

    Implement CORS

--- Nigel/src/app.ts
+++ Nigel/src/app.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import cors from 'cors';
 import morgan from 'morgan';
 import http from 'http';
 import logger from './logger';
@@ -7,6 +8,9 @@ import oneSampleTestRoutes from "./oneSampleTestRoutes";
 async function startServer() {
     try {
         const app = express();
+
+        const corsOrigin = process.env.CORS_ORIGIN || '*';
+        app.use(cors({ origin: corsOrigin }));
 
         const morganFormat = ":remote-addr - :remote-user \":method :url HTTP/:http-version\" :status :res[content-length] \":referrer\" \":user-agent\"";
         app.use(morgan(morganFormat, {
